7d3764bab1de7bc1ea635f30c07b4dede533821e
Michael Sankovich <misankovich@gmail.com>
2015-10-27 00:48:36 -0700

    split up config and controllers. profile info

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -6,24 +6,7 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
   .state('/', { url: '/', templateUrl: 'views/home.ejs', controller: 'mainCtrl' })
   .state('settings', { url: '/settings', templateUrl: 'views/settings.ejs', controller: 'mainCtrl' })
   .state('profile', { url: '/profile/{userId}', templateUrl: 'views/profile.ejs', controller: 'mainCtrl' })
-  .state('institutions', { url: '/institutions', templateUrl: 'views/institutions.ejs', controller: 'mainCtrl' })
+  .state('institutions', { url: '/institutions', templateUrl: 'views/institutions.ejs', controller: 'institutionCtrl' })
   .state('login', { url: '/login', templateUrl: 'views/login.ejs', controller: 'mainCtrl' })
   .state('register', { url: '/register', templateUrl: 'views/register.ejs', controller: 'mainCtrl' });
 }]);
-
-app.controller('mainCtrl', function($scope, $state, $http){
-  $http.get('http://localhost:3000/user').success(function(user) {
-    if(user) {
-      console.log("user", user);
-      $scope.currentUser = user.username;
-      $scope.userId = user._id;
-      console.log($scope.userId);
-    }
-  });
-  $http.get('http://localhost:3000/institutions').success(function(institution) {
-      console.log("institution", institution);
-      $scope.institutions = institution;
-      console.log($scope.institutions);
-  });
-
-});
