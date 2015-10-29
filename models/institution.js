f6c7da4f0a4065e30d332aedb71eaaac9769a2da
Charles Watson <nchlswtsn@gmail.com>
2015-10-29 10:19:30 -0700

    added slight bio styling

--- models/institution.js
+++ models/institution.js
@@ -4,13 +4,15 @@ var institution = Mongoose.Schema({
   //added when user adds a new one (upsert?)
   name: {type: String, unique: true},
   alumni: [],
-  picUrl: {type: String, default: "http://edukasyon.ph/assets/institution-default-logo-e8e6823adee81942cd3d2c9fd0161f87.png"},
+  picUrl: {type: String, default: 'http://edukasyon.ph/assets/institution-default-logo-e8e6823adee81942cd3d2c9fd0161f87.png'},
   events: [{
     when: Date,
     whoCanAttend: String,
     price: Number,
-    address: String
-  }]
+    address: String,
+  },
+ ],
+  tag: String,
 });
 
 var Institution = Mongoose.model('Institution', institution);
