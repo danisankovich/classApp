fb34682953d0bdb2ecdc6630bf93727b9e77a085
Michael Sankovich <misankovich@gmail.com>
2015-10-31 14:58:20 -0700

    send and show one message ctrl initial setup. further mail routes.

--- public/javascripts/mailCtrl.js
+++ public/javascripts/mailCtrl.js
@@ -6,10 +6,31 @@ app.controller('mailCtrl', function($scope, $state, $http){
       console.log("messages", messages);
       messages.forEach(function(e) {
         if (e.recipient === $scope.user._id) {
-          $scope.messages.push(e);
+          $http.get("http://localhost:3000/mail/sender").success(function(sender) {
+            e.senderName = sender.fullName;
+            $scope.messages.push(e);
+          });
         }
-        //before escaping, have to do a find user by id for all messages.
       });
     });
   });
+
+  $scope.showOneMessage = function() {
+    console.log(this);
+    var msgId = this.message._id;
+    $state.go('mail/' + {msgId: msgId});
+  };
+
+  $scope.sendMailForm = function() {
+    if ($scope.showNameForm === true) { return $scope.showNameForm = false;
+    } else { return $scope.showNameForm = true; }
+  };
+
+  $scope.sendMail = function() {
+    //this may have to be in other controllers, based on how we are going to access another user's id.
+    $http.post("http://localhost:3000/send/:id").success(function(sentMessage) {
+      console.log(sentMessage);
+      swal("Message Sent!", "Your message has been sent and will be delivered shortly!", "success");
+    });
+  };
 });
