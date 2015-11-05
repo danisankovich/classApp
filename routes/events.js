938bf8df4bc7ecb1d2b4e3c74751a61f7f0561e0
Michael Sankovich <misankovich@gmail.com>
2015-11-05 00:24:52 -0800

    cleanup

--- routes/events.js
+++ routes/events.js
@@ -22,5 +22,22 @@ router.post('/new/:id', function(req, res) {
       console.log(newEvent);
   });
 });
+router.post('/new/:id', function(req, res) {
+  Institution.findByIdAndUpdate(
+    req.params.id,
+    {$push: {"events": {
+      name: req.body.name,
+      creatorId: req.user._id,
+      picUrl: req.body.picUrl,
+      when: req.body.when,
+      description: req.body.description,
+      whoCanAttend: req.body.whoCanAttend,
+      price: req.body.price
+    }}},
+    {safe: true, upsert: true},
+    function(err, newEvent) {
+      console.log(newEvent);
+  });
+});
 
 module.exports = router;
