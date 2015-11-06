3931a1a8c9344a6da9048b1df4e62597651136f3
Michael Liang <michaelpliang@gmail.com>
2015-11-06 00:58:49 -0800

    fixed cors again

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ var Message = require('../models/message');
 
 router.post('/:id', function(req, res, next) {
   console.log("hey bud", req.params.id);
-  console.log('yaayay');
   User.findById("56396ff8afa0b8f92c69d141", function(err, user) {
     console.log(user);
     res.json(user);
