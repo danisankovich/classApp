03979abba61de9394a22f772af51ff4ef9dfc45a
Michael Liang <michaelpliang@gmail.com>
2015-11-05 17:01:10 -0800

    login and register errors added

--- public/javascripts/controllers/controller.js
+++ public/javascripts/controllers/controller.js
@@ -1,29 +1,56 @@
-app.controller('mainCtrl', function($scope, $state, $http){
+app.controller('mainCtrl', function ($scope, $state, $http) {
+
   $http.get('http://localhost:3000/user')
-    .success(function(user) {
-      if(user) {
+    .success(function (user) {
+      if (user) {
         console.log("user", user);
         $scope.user = user;
         $scope.currentUser = user.username;
         $scope.unreadMessages = user.unreadMessages;
         $scope.userId = user._id;
         instvsInsts(user);
+        $scope.test = 'test';
       }
-    })
+    }).catch(function (error) {
+      console.log(error);
+    });
 
-  var instvsInsts = function(user) {
+  $scope.register = function (user) {
+    console.log(user);
+    $http.post('http://localhost:3000/register', user)
+      .success(function (data) {
+        console.log(data);
+      })
+      .error(function (error) {
+        console.log(error);
+        $scope.error = error.message;
+      });
+  };
+
+  $scope.login = function (user) {
+    console.log(user);
+    $http.post('http://localhost:3000/login', user)
+      .success(function (data) {
+        console.log(data);
+      })
+      .error(function (error) {
+        console.log(error);
+        $scope.error = error.message;
+      })
+  };
+
+  var instvsInsts = function (user) {
     if (user.institutions.length > 1) {
       $scope.memberOfOne = false;
-    }
-    else {
+    } else {
       $scope.memberOfOne = true;
     }
   };
 });
 
-app.controller('institutionCtrl', function($scope, $state, $http){
+app.controller('institutionCtrl', function ($scope, $state, $http) {
   $scope.alumni = [];
-  $http.get('http://localhost:3000/institutions').success(function(institution) {
+  $http.get('http://localhost:3000/institutions').success(function (institution) {
     console.log("institution", institution);
     $scope.institutions = institution;
     console.log($scope.institutions);
@@ -38,9 +65,11 @@ app.controller('institutionCtrl', function($scope, $state, $http){
   //     $state.go('login')
   //   });
 
-  $scope.showOneInstitute = function() {
+  $scope.showOneInstitute = function () {
     instituteId = this.institution._id;
     console.log(instituteId);
-    $state.go('institute', {instituteId:instituteId});
+    $state.go('institute', {
+      instituteId: instituteId
+    });
   };
 });
