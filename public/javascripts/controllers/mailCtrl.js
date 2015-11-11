8e5f077b8be87dcf1d165e314087962cd866ac1c
Michael Liang <michaelpliang@gmail.com>
2015-11-11 12:40:30 -0800

    mongolab configs added

--- public/javascripts/controllers/mailCtrl.js
+++ public/javascripts/controllers/mailCtrl.js
@@ -2,12 +2,12 @@ app.controller('mailCtrl', function($scope, $state, $http){
   $scope.messages = [];
   $scope.friends = [];
   $scope.friendsHidden = false;
-  $http.get('http://localhost:3000/user').success(function(user) {
+  $http.get('https://alumni-network.herokuapp.com/user').success(function(user) {
     $scope.user = user;
-    $http.get('http://localhost:3000/mail/mymail').success(function(messages) {
+    $http.get('https://alumni-network.herokuapp.com/mail/mymail').success(function(messages) {
         messages.forEach(function(e) {
           if (e.recipientId === $scope.user._id) {
-            $http.get("http://localhost:3000/mail/sender/" + e.senderId).success(function(sender) {
+            $http.get("https://alumni-network.herokuapp.com/mail/sender/" + e.senderId).success(function(sender) {
               e.senderName = sender.fullName;
               $scope.messages.push(e);
               console.log(messages);
@@ -16,7 +16,7 @@ app.controller('mailCtrl', function($scope, $state, $http){
         });
       });
       user.friends.forEach(function(friend) {
-        $http.get('http://localhost:3000/friends/' + friend.friendId, friend).success(function(friend) {
+        $http.get('https://alumni-network.herokuapp.com/friends/' + friend.friendId, friend).success(function(friend) {
           $scope.friends.push(friend);
           console.log("my friends", $scope.friends);
         });
@@ -46,7 +46,7 @@ app.controller('mailCtrl', function($scope, $state, $http){
     var thisUserId = $scope.friendId;
     console.log("thisuserid", thisUserId);
     console.log("mymessage", message);
-    $http.post("http://localhost:3000/mail/send/" + thisUserId, message).success(function(sentMessage) {
+    $http.post("https://alumni-network.herokuapp.com/mail/send/" + thisUserId, message).success(function(sentMessage) {
       console.log(sentMessage);
       swal("Message Sent!", "Your message has been sent and will be delivered shortly!", "success");
     });
@@ -56,9 +56,9 @@ app.controller('mailCtrl', function($scope, $state, $http){
 app.controller('oneMsgCtrl', function($scope, $state, $http){
   var thisUserId;
   console.log($state.params.msgId);
-  $http.get('http://localhost:3000/mail/onemessage/' + $state.params.msgId).success(function(message) {
+  $http.get('https://alumni-network.herokuapp.com/mail/onemessage/' + $state.params.msgId).success(function(message) {
     console.log(message);
-    $http.get("http://localhost:3000/mail/sender/" + message.senderId).success(function(sender) {
+    $http.get("https://alumni-network.herokuapp.com/mail/sender/" + message.senderId).success(function(sender) {
       message.senderName = sender.fullName;
       $scope.message = message;
       console.log("this", $scope.message);
@@ -73,7 +73,7 @@ app.controller('oneMsgCtrl', function($scope, $state, $http){
   $scope.replyUser = function(message) {
     console.log("thisuserid", thisUserId);
     console.log("mymessage", message);
-    $http.post("http://localhost:3000/mail/reply/" + thisUserId, message).success(function(sentMessage) {
+    $http.post("https://alumni-network.herokuapp.com/mail/reply/" + thisUserId, message).success(function(sentMessage) {
       console.log(sentMessage);
       swal("Message Sent!", "Your message has been sent and will be delivered shortly!", "success");
     });
